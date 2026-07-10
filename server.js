8f447ab30f5240d5ca45d190c236c323eb9c8f7e
isingomaarthur30-design <isingomaarthur30@gmail.com>
2026-07-10 15:29:41 +0300

    allow all origins cors

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  // origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  origin:"*",
   credentials: true
 }));
 app.use(express.json());
